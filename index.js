7172c4643f10700cd8e3ccdf93fe43e6d026cf2e
Greg Larrenaga <greg.larrenaga@thinksolid.com>
2014-12-14 22:04:56 -0800

    Cleaning this thing out., setting up bare minumum express server.

---
+++ index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var express = require('express'),
+    app = express();
+
+app.engine('jade', require('jade').__express);
+app.set('view engine', 'jade');
+app.set('views', './views');
+app.set('view options', {layout: true});
+app.use(express.static('./public'));
+
+app.get('*', function(req, res) {
+    res.render('landing');
+});
+
+app.listen(3000);
