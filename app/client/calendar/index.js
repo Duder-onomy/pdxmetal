919866d2a01ab512ecae5a902ae4a653e4e7a86d
Greg Larrenaga <greg.larrenaga@thinksolid.com>
2014-12-21 20:35:28 -0800

    Some really bad zooming...but I am def learning.

--- app/client/calendar/index.js
+++ app/client/calendar/index.js
@@ -1,84 +1,128 @@
 (function(d3, $, calendarData) {
     'use strict';
 
-    var year,
-        months,
-        days;
+    var daySize = '20';
 
-    year = d3.select('#stage')
-        .append('svg');
+    // Append an svg to the stage.
+    var svg = d3.select('#stage').append('svg');
 
-    months = year.selectAll('.month')
-        .data(calendarData)
+    var g = svg.append('g');
+
+    var zoom = d3.behavior.zoom()
+        .translate([0, 0])
+        .scale(1)
+        .scaleExtent([1, 8])
+        .on('zoom', zoomed);
+
+    // Append 10 rectangles to the svg;
+    g.selectAll('.day')
+        .data(d3.range(1, 20))
         .enter()
-        .append('svg:g')
+        .append('rect')
+        .attr('width', daySize)
+        .attr('height', daySize)
+        .attr('x', function(d) {
+            return d * daySize;
+        })
+        /*.attr('y', function(d) {
+            return d * daySize;
+        })*/
+        .attr('class', 'day')
+        .on('click', function(d) {
+
+            /*if (active.node() === this) {
+                return reset();
+            };*/
+            /*active.classed('active', false);*/
+            /*active = d3.select(this).classed('active', true);*/
+            var bounds = this.getBBox();
+            /*var dx = x;
+            var dy = bounds[1][1] - bounds[0][1];
+            var x = (bounds[0][0] + bounds[1][0]) / 2;
+            var y = (bounds[0][1] + bounds[1][1]) / 2;*/
+
+            var scale = 0.9 / Math.max(bounds.width / 960, bounds.height / 500),
+                translate = [960 / 2 - scale * bounds.width, 500 / 2 - scale * bounds.height];
+
+            g.transition()
+                .duration(750)
+                .call(zoom.translate(translate).scale(scale).event);
+        });
+
+    function zoomed() {
+        g.style('stroke-width', 1.5 / d3.event.scale + 'px');
+        g.attr('transform', 'translate(' + d3.event.translate + ')scale(' + d3.event.scale + ')');
+    }
+
+
+    /*var svg = d3.select('#stage').selectAll('svg')
+        .data(d3.range(1, 20))
+        .enter()
+            .append('svg')*/
+            /*.attr('width', width)*/
+            /*.attr('height', height)*/
+            /*.attr('class', 'RdYlGn')*/
+            /*.append('g');*/
+            /*.attr('transform', 'translate(' + ((width - cellSize * 53) / 2) + ',' + (height - cellSize * 7 - 1) + ')');*/
+
+
+
+    /*var width = 960,
+        height = 136,
+        cellSize = 17; // cell size
+
+    var day = d3.time.format('%w'),
+        week = d3.time.format('%U'),
+        percent = d3.format('.1%'),
+        format = d3.time.format('%Y-%m-%d');
+
+    var color = d3.scale.quantize()
+        .domain([-0.05, 0.05])
+        .range(d3.range(11).map(function(d) { return 'q' + d + '-11'; }));
+
+    var svg = d3.select('#stage').selectAll('svg')
+        .data(d3.range(2013, 2015))
+        .enter().append('svg')
+        .attr('width', width)
+        .attr('height', height)
+        .attr('class', 'RdYlGn')
+        .append('g')
+        .attr('transform', 'translate(' + ((width - cellSize * 53) / 2) + ',' + (height - cellSize * 7 - 1) + ')');
+
+    svg.append('text')
+        .attr('transform', 'translate(-6,' + cellSize * 3.5 + ')rotate(-90)')
+        .style('text-anchor', 'middle')
+        .text(function(d) { return d; });
+
+    var rect = svg.selectAll('.day')
+        .data(function(d) { return d3.time.days(new Date(d, 0, 1), new Date(d + 1, 0, 1)); })
+        .enter().append('rect')
+        .attr('class', 'day')
+        .attr('width', cellSize)
+        .attr('height', cellSize)
+        .attr('x', function(d) { return week(d) * cellSize; })
+        .attr('y', function(d) { return day(d) * cellSize; })
+        .datum(format);
+
+    rect.append('title')
+        .text(function(d) { return d; });
+
+    svg.selectAll('.month')
+        .data(function(d) { return d3.time.months(new Date(d, 0, 1), new Date(d + 1, 0, 1)); })
+        .enter().append('path')
         .attr('class', 'month')
-        .style('fill', '#fff')
-        .style('stroke', '#555');
+        .attr('d', monthPath);
 
-    days = months.selectAll('.day')
-        .data(function(month) { return month; })
-        .enter()
-        .append('svg:rect');
+    function monthPath(t0) {
+        var t1 = new Date(t0.getFullYear(), t0.getMonth() + 1, 0),
+            d0 = +day(t0), w0 = +week(t0),
+            d1 = +day(t1), w1 = +week(t1);
 
-
-    /*cells = rows.selectAll('.cell')
-        .data(function (d) { return d; })
-        .enter().append('svg:rect')
-        .attr('class', 'cell')
-        .attr('x', function(d) { return d.x; })
-        .attr('y', function(d) { return d.y; })
-        .attr('width', function(d) { return d.width; })
-        .attr('height', function(d) { return d.height; })
-        .on('mouseover', function() {
-            d3.select(this)
-                .style('fill', '#0F0');
-        })
-        .on('mouseout', function() {
-            d3.select(this)
-                .style('fill', '#FFF');
-        })
-        .on('click', function() {
-            console.log(d3.select(this));
-        })
-        .style('fill', '#FFF')
-        .style('stroke', '#555');*/
-
-    /*function randomData(gridWidth, gridHeight, square) {
-        var data = new Array();
-        var gridItemWidth = gridWidth / 24;
-        var gridItemHeight = (square) ? gridItemWidth : gridHeight / 7;
-        var startX = gridItemWidth / 2;
-        var startY = gridItemHeight / 2;
-        var stepX = gridItemWidth;
-        var stepY = gridItemHeight;
-        var xpos = startX;
-        var ypos = startY;
-        var newValue = 0;
-        var count = 0;
-
-        for (var index_a = 0; index_a < 7; index_a++)
-        {
-            data.push(new Array());
-            for (var index_b = 0; index_b < 24; index_b++)
-            {
-                newValue = Math.round(Math.random() * (100 - 1) + 1);
-                data[index_a].push({
-                    time: index_b,
-                    value: newValue,
-                    width: gridItemWidth,
-                    height: gridItemHeight,
-                    x: xpos,
-                    y: ypos,
-                    count: count
-                });
-                xpos += stepX;
-                count += 1;
-            }
-            xpos = startX;
-            ypos += stepY;
-        }
-        return data;
+        return 'M' + (w0 + 1) * cellSize + ',' + d0 * cellSize +
+            'H' + w0 * cellSize + 'V' + 7 * cellSize +
+            'H' + w1 * cellSize + 'V' + (d1 + 1) * cellSize +
+            'H' + (w1 + 1) * cellSize + 'V' + 0 +
+            'H' + (w0 + 1) * cellSize + 'Z';
     }*/
 
 })(window.d3, window.$, window.calendarData);
